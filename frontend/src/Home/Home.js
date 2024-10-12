00d0f0917f6937ce4117a2b5f4a32d549c371c18
TiAn <siebert@student.ethz.ch>
2024-10-12 11:09:11 +0200

    Create other grid on home page - create basic elements of the upper/interaction area

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,7 +1,26 @@
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
+import ToggleOptions from '../Components/ToggleOptions/ToggleOptions.js'
 
 function Home() {
-
+    const handleToggle = {}
+    const options = ["Newest", "Best"];
+    return (
+        <div class = "outer-grid-container">
+            <div class="interactionArea">
+                <div class="searchBar">
+                    <input id="searchBar" type="text" placeholder="Search here"/>
+                </div>
+                <ToggleOptions onToggle={handleToggle} options={options}>
+                </ToggleOptions>
+                <button class="bigBadBunnyButton">
+                    <label>Post new Question</label>
+                </button>
+            </div>
+            <div class="questionCardArea">
+                Second
+            </div>
+        </div>
+    )
 }
 export default Home;
