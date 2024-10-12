45d7a3f42864576de31d452adcf295558368b0fa
TiAn <siebert@student.ethz.ch>
2024-10-12 03:25:20 +0200

    Co-size ETH logo and logout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <img src="eth_logo_kurz_pos.png" alt="Background" />
+      <img className= "eth-logo" src="eth_logo_kurz_pos.png" alt="Background" />
       <img className="logout-icon" src="logout_icon.svg" alt="Logout" />
       <App />
     </Router>
