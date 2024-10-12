f1c17683df9a52cbba30174ed4b52a4a8f987559
Anna <95139748+aniasz27@users.noreply.github.com>
2024-10-12 10:56:42 +0200

    Authentication (#11)
    * Add middleware
    
    * print email
    
    * Add tokens
    
    * Add user id
    
    * Read user id
    
    * fix user id
    
    * Fix user id
    
    * add user id to controller
    
    * Print req
    
    * Add body
    
    * delete if statements
    
    * Hardcode values

---
+++ backend/src/middleware/authentication.middleware.ts
@@ -0,0 +1,49 @@
+import { Middleware, ExpressMiddlewareInterface } from "routing-controllers";
+import { Request, Response, NextFunction } from "express";
+import {Service} from "typedi";
+import dotenv from 'dotenv';
+import {sign} from "jsonwebtoken";
+import prisma from '../../prisma';
+
+dotenv.config();
+
+const secretKey: string = process.env.JWT_SECRET_KEY!;
+
+@Service()
+@Middleware({ type: 'before' })
+export class AuthenticationMiddleware implements ExpressMiddlewareInterface {
+
+    async use(req: Request, res: Response, next: NextFunction): Promise<void> {
+        if (res.locals.needAuthentication) {
+            // const email: string = req.headers['x-authentik-email'] as string;
+            // TODO: change at the end
+            const email = "xxx@student.ethz.ch"
+
+            let user = await prisma.account.findUnique({ where: { email } });
+            if (!user) {
+                // TODO: change at the end
+                // const name: string = req.headers['x-authentik-name'] as string;
+                // const username: string = req.headers['x-authentik-username'] as string;
+                const name = "xxx"
+                const username = "xxx"
+
+                user = await prisma.account.create({
+                    data: {
+                        email: email,
+                        name: name,
+                        username: username,
+                    }
+                })
+            }
+
+            // @ts-ignore
+            req.userId = user.id;
+
+            const token = sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
+
+            res.setHeader('Authorization', `Bearer ${token}`);
+            res.locals.token = token;
+        }
+        next()
+    }
+}
