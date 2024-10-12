9a806b288d8d2f9a059bf8b0d2b7facd8d3ee763
Kaloyan Fachikov <kalo_2002@abv.bg>
2024-10-13 01:48:22 +0200

    fix: authentication

--- backend/src/middleware/authentication.middleware.ts
+++ backend/src/middleware/authentication.middleware.ts
@@ -2,7 +2,7 @@ import { Middleware, ExpressMiddlewareInterface } from "routing-controllers";
 import { Request, Response, NextFunction } from "express";
 import { Service } from "typedi";
 import dotenv from "dotenv";
-import { sign } from "jsonwebtoken";
+import { sign, verify } from "jsonwebtoken";
 import prisma from "../../prisma";
 
 dotenv.config();
@@ -14,31 +14,43 @@ const secretKey: string = process.env.JWT_SECRET_KEY!;
 export class AuthenticationMiddleware implements ExpressMiddlewareInterface {
   async use(req: Request, res: Response, next: NextFunction): Promise<void> {
     if (res.locals.needAuthentication) {
-      // const email: string = req.headers['x-authentik-email'] as string;
-      // TODO: change at the end
-      const email = "xxx@student.ethz.ch";
-
-      let user = await prisma.account.findUnique({ where: { email } });
-      if (!user) {
+      let token: string;
+      
+      if (req.headers["authentication"]) {
+        token = req.headers["authentication"] as string;
+        token = token.split(" ")[1];
+        try {
+          const payload = verify(token, secretKey);
+          // @ts-ignore
+          req.userId = payload.userId;
+        } catch (error) {
+          res.status(401).send("Unauthorized");
+          return;
+        }
+      } else {
+        const email: string = req.headers['x-authentik-email'] as string;
         // TODO: change at the end
-        // const name: string = req.headers['x-authentik-name'] as string;
-        // const username: string = req.headers['x-authentik-username'] as string;
-        const name = "xxx";
-        const username = "xxx";
-
-        user = await prisma.account.create({
-          data: {
-            email: email,
-            name: name,
-            username: username,
-          },
-        });
+  
+        let user = await prisma.account.findUnique({ where: { email } });
+        if (!user) {
+          // TODO: change at the end
+          const name: string = req.headers['x-authentik-name'] as string;
+          const username: string = req.headers['x-authentik-username'] as string;
+  
+          user = await prisma.account.create({
+            data: {
+              email: email,
+              name: name,
+              username: username,
+            },
+          });
+        }
+  
+        // @ts-ignore
+        req.userId = user.id;
+  
+        token = sign({ userId: user.id }, secretKey);
       }
-
-      // @ts-ignore
-      req.userId = user.id;
-
-      const token = sign({ userId: user.id }, secretKey, { expiresIn: "1h" });
 
       res.setHeader("Authorization", `Bearer ${token}`);
       res.locals.token = token;
